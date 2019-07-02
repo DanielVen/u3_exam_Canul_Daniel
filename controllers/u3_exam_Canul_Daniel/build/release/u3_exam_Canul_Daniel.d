974b64065f29abed391179f5db78109eaa93cb87
Daniel <al1701021@upy.edu.mx>
2019-07-02 15:41:32 -0500

    twodirections already set in the robot, forward and backward working good

---
+++ controllers/u3_exam_Canul_Daniel/build/release/u3_exam_Canul_Daniel.d
@@ -0,0 +1,7 @@
+build/release/u3_exam_Canul_Daniel.o: u3_exam_Canul_Daniel.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/keyboard.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h
