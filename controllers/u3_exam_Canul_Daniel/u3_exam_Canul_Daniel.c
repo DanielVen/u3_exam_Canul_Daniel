774b72fa429549ce7e6b0fda89edfd223a765548
Daniel <al1701021@upy.edu.mx>
2019-07-04 12:38:26 -0500

    automatic and manual functions activate, the robot has all the especifications asked in slack

--- controllers/u3_exam_Canul_Daniel/u3_exam_Canul_Daniel.c
+++ controllers/u3_exam_Canul_Daniel/u3_exam_Canul_Daniel.c
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
    wb_motor_set_position(wheel_right, INFINITY);
    wb_motor_set_position(wheel_left, INFINITY);
    wb_motor_set_position(wheel_back, INFINITY);
-   
+
    //Encoder devices
    WbDeviceTag Encoder1 = wb_robot_get_device("encoder1");
    WbDeviceTag Encoder2 = wb_robot_get_device("encoder2");
@@ -60,30 +60,173 @@ int main(int argc, char **argv)
    wb_position_sensor_enable(Encoder1, TIME_STEP);
    wb_position_sensor_enable(Encoder2, TIME_STEP);
    wb_position_sensor_enable(Encoder3, TIME_STEP);
-      
+
    float Enco1=0;
    float Enco2=0;
    float Enco3=0;
-   
+
    //distance sensor devices
    WbDeviceTag dist_sensorR1 = wb_robot_get_device("dist_sensor1");
    WbDeviceTag dist_sensorL2 = wb_robot_get_device("dist_sensor2");
 
    wb_distance_sensor_enable(dist_sensorR1, TIME_STEP);
    wb_distance_sensor_enable(dist_sensorL2, TIME_STEP);
-   
+
    float ds_Right1=0;
    float ds_Left2=0;
-   
+
    //Other variables
    float compare=0;
    float turn_right=0;
    float turn_left=0;
-   
+   int w=0;
+   int g=0;
   /* main loop
    * Perform simulation steps of TIME_STEP milliseconds
    * and leave the loop when the simulation is over
    */
+   void manual(){
+
+        //Distance Sensor Read
+        ds_Right1 = wb_distance_sensor_get_value(dist_sensorR1);
+        ds_Left2 = wb_distance_sensor_get_value(dist_sensorL2);
+
+        Enco1 = wb_position_sensor_get_value(Encoder1);
+        Enco2 = wb_position_sensor_get_value(Encoder2);
+        Enco3 = wb_position_sensor_get_value(Encoder3);
+
+        printf("distance_right: %lf\r\n", ds_Right1);
+        printf("distance_left: %lf\r\n", ds_Left2);
+
+        printf("Encoder1: %lf\r\n", Enco1);
+        printf("Encoder2: %lf\r\n", Enco2);
+        printf("Encoder3: %lf\r\n", Enco3);
+        printf("Comparador: %lf\n",compare );
+        // look for obsctacles
+        //bool right_obst = dist_right <= OBSTACLE_DIST;
+       // bool left_obst = dist_left <= OBSTACLE_DIST;
+
+
+
+
+        if(pressed_key == WB_KEYBOARD_UP){
+
+          wb_motor_set_velocity(wheel_left, 6.36);
+          wb_motor_set_velocity(wheel_right, -6.36);
+          wb_motor_set_velocity(wheel_back, 0);
+        }
+        else if(pressed_key == WB_KEYBOARD_DOWN){
+          wb_motor_set_velocity(wheel_left, -6.36);
+          wb_motor_set_velocity(wheel_right, 6.36);
+          wb_motor_set_velocity(wheel_back, 0);
+
+        }
+        else if(pressed_key == WB_KEYBOARD_LEFT){
+          wb_motor_set_velocity(wheel_left, 0);
+          wb_motor_set_velocity(wheel_right, -6.36);
+          wb_motor_set_velocity(wheel_back, 6.36);
+
+        }
+        else if(pressed_key == WB_KEYBOARD_RIGHT){
+          wb_motor_set_velocity(wheel_left, 0);
+          wb_motor_set_velocity(wheel_right, 6.36);
+          wb_motor_set_velocity(wheel_back, -6.36);
+
+
+        }
+        else if(pressed_key == 'S' ){
+          compare = Enco1 + 0.785398;
+          turn_left = 1;
+        }
+
+        else if(turn_left == 1){
+
+          if(Enco1 <= compare){
+          wb_motor_set_velocity(wheel_left, 6.36);
+          wb_motor_set_velocity(wheel_right, 6.36);
+          wb_motor_set_velocity(wheel_back, 6.36);
+        }
+        else{
+          wb_motor_set_velocity(wheel_left, 0);
+          wb_motor_set_velocity(wheel_right, 0);
+          wb_motor_set_velocity(wheel_back, 0);
+          turn_left = 0;
+        }
+
+      }
+
+        else if(pressed_key == 'A' ){
+        compare = Enco1 - 0.785398;
+        turn_right = 1;
+
+        }
+         else if(turn_right == 1){
+
+          if(Enco1 >= compare){
+          wb_motor_set_velocity(wheel_left, -6.36);
+          wb_motor_set_velocity(wheel_right,-6.36);
+          wb_motor_set_velocity(wheel_back, -6.36);
+        }
+        else{
+          wb_motor_set_velocity(wheel_left, 0);
+          wb_motor_set_velocity(wheel_right, 0);
+          wb_motor_set_velocity(wheel_back, 0);
+          turn_right = 0;
+        }
+
+        }
+        else{
+          wb_motor_set_velocity(wheel_left, 0);
+          wb_motor_set_velocity(wheel_right, 0);
+          wb_motor_set_velocity(wheel_back, 0);
+        }
+      }
+
+  void automatico()
+  {
+
+    //Distance Sensor Read
+    ds_Right1 = wb_distance_sensor_get_value(dist_sensorR1);
+    ds_Left2 = wb_distance_sensor_get_value(dist_sensorL2);
+
+    Enco1 = wb_position_sensor_get_value(Encoder1);
+    Enco2 = wb_position_sensor_get_value(Encoder2);
+    Enco3 = wb_position_sensor_get_value(Encoder3);
+
+    printf("distance_right: %lf\r\n",ds_Right1);
+    printf("distance_left: %lf\r\n", ds_Left2);
+
+  //  printf("Encoder1: %lf\r\n", Enco1);
+  //  printf("Encoder2: %lf\r\n", Enco2);
+  //  printf("Encoder3: %lf\r\n", Enco3);
+  //  printf("Comparador: %lf\n",compare );
+  // Look for obsctacles
+  // bool right_obst = dist_right <= OBSTACLE_DIST;
+  // bool left_obst = dist_left <= OBSTACLE_DIST;
+
+    wb_motor_set_velocity(wheel_left,  6.36);
+    wb_motor_set_velocity(wheel_right, -6.36);
+    wb_motor_set_velocity(wheel_back, 0);
+
+    if(ds_Left2 < ds_Right1 && ds_Left2 < 200){
+
+     wb_motor_set_velocity(wheel_left, 6.36);
+     wb_motor_set_velocity(wheel_right, 0);
+     wb_motor_set_velocity(wheel_back, -6.36);
+    }
+    else if(ds_Left2 > ds_Right1 && ds_Right1 < 200){
+     wb_motor_set_velocity(wheel_left, -6.36);
+     wb_motor_set_velocity(wheel_right, 0);
+     wb_motor_set_velocity(wheel_back, 6.36);
+    }
+  }
+
+   wb_motor_set_velocity(wheel_left, 0);
+   wb_motor_set_velocity(wheel_right, 0);
+   wb_motor_set_velocity(wheel_back, 0);
+
+
+
   while (wb_robot_step(TIME_STEP) != -1) {
 
     /*
@@ -94,17 +237,17 @@ int main(int argc, char **argv)
      //Distance sensor read
     ds_Right1 = wb_distance_sensor_get_value(dist_sensorR1);
     printf("First Distance sensor %lf\r\n", ds_Right1);
-    
+
     ds_Left2 = wb_distance_sensor_get_value(dist_sensorL2);
     printf("Second Distance sensor %lf\r\n", ds_Left2);
-    
+
     //Position Sensor read
     Enco1 = wb_position_sensor_get_value(Encoder1);
     printf("Encoder 1 value: %f\r\n", Enco1);
-    
+
     Enco2 = wb_position_sensor_get_value(Encoder2);
     printf("Encoder 2 value: %lf\r\n", Enco2);
-    
+
     Enco3 = wb_position_sensor_get_value(Encoder3);
     printf("Encoder 3 value: %lf\r\n", Enco3);
 
@@ -114,77 +257,26 @@ int main(int argc, char **argv)
      * Enter here functions to send actuator commands, like:
      * wb_differential_wheels_set_speed(100.0,100.0);
      */
-  if(pressed_key == WB_KEYBOARD_UP)
-  {
-     wb_motor_set_velocity(wheel_right,-6.36);
-     wb_motor_set_velocity(wheel_left, 6.36);
-     wb_motor_set_velocity(wheel_back, 0);
-  }
-  else if(pressed_key == WB_KEYBOARD_DOWN){
-     wb_motor_set_velocity(wheel_right, 6.36);
-     wb_motor_set_velocity(wheel_left, -6.36);
-     wb_motor_set_velocity(wheel_back, 0);
+     if(pressed_key == 'W')
+     {
+       w = 1;
+       g = 0;
+     }
 
-  }
-  else if(pressed_key == WB_KEYBOARD_RIGHT){
-     wb_motor_set_velocity(wheel_right, 6.36);
-     wb_motor_set_velocity(wheel_left, 0);
-     wb_motor_set_velocity(wheel_back, -6.36);
-  }
-  else if(pressed_key == WB_KEYBOARD_LEFT){
-     wb_motor_set_velocity(wheel_right,0);
-     wb_motor_set_velocity(wheel_left,-6.36);
-     wb_motor_set_velocity(wheel_back,6.36);
-  }
-  
+     else if (pressed_key == 'G')
+     {
+       g = 1;
+       w = 0;
+     }
+     printf("W: %i   ",w);
+     printf("G: %i\n",g);
 
-   else if(pressed_key == 'S' ){
-    compare = Enco1 + 0.785398;
-    turn_left = 1;
-  }
+     if(w == 1)
+     manual();
 
-  else if(turn_left == 1){
-
-    if(Encoder1 <= compare){
-    wb_motor_set_velocity(wheel_left, 6.36);
-    wb_motor_set_velocity(wheel_right, 6.36);
-    wb_motor_set_velocity(wheel_back, 6.36);
-  }
-  else{
-    wb_motor_set_velocity(wheel_left, 0);
-    wb_motor_set_velocity(wheel_right, 0);
-    wb_motor_set_velocity(wheel_back, 0);
-    turn_left = 0;
-  }
-
-}
-
-  else if(pressed_key == 'A' ){
-  compare = Enco1 - 0.785398;
-  turn_right = 1;
-
-  }
-   else if(turn_right == 1){
-
-    if(Encoder1 >= compare){
-    wb_motor_set_velocity(wheel_left, -6.36);
-    wb_motor_set_velocity(wheel_right, -6.36);
-    wb_motor_set_velocity(wheel_back, -6.36);
-  }
-  else{
-    wb_motor_set_velocity(wheel_left, 0);
-    wb_motor_set_velocity(wheel_right, 0);
-    wb_motor_set_velocity(wheel_back, 0);
-    turn_right = 0;
-  }
-  }
-  else {
-     wb_motor_set_velocity(wheel_right,0);
-     wb_motor_set_velocity(wheel_left, 0);
-     wb_motor_set_velocity(wheel_back, 0);
-  }
-
-  };
+     if(g == 1)
+     automatico();
+     };
 
   /* Enter your cleanup code here */
 
